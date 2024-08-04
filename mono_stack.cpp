2da83f750efba50464fd4dc680664b3c3c8c7057
selene <vaporflyb@gmail.com>
2024-08-04 05:27:04 -0400

    mono_stack

---
+++ mono_stack.cpp
@@ -0,0 +1,50 @@
+/*
+    Implementation for a monotonic decreasing stack in C++.
+    We traverse the array.
+    While the stack is not empty and the TOP of the stack is less than
+    the current element, then pop.
+
+    Next, construct the reuslt array and push the current element into the stack.
+ */
+
+#include <iostream>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+vector<int> monotonicDecreasing(vector<int>& nums) {
+    int n = nums.size();
+    stack<int> st;
+    vector<int> result(n);
+
+    for (int i = 0; i < n; ++i) {
+        while (!st.empty() && st.top() < nums[i]) {
+            st.pop();
+        }
+
+        if (!st.empty()) {
+            result[i] = st.top();
+        }
+        else {
+            result[i] = -1;
+        }
+
+        st.push(nums[i]);
+    }
+    return result;
+}
+
+int main() {
+    vector<int> nums = {};
+    vector<int> result = monotonicDecreasing(nums);
+    cout << "Monotonic decreasing stack: ";
+    for (int val : result) {
+        cout << val << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
+
+}
